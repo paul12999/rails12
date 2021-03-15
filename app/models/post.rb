4f61938f93b25732610b00a1c6ca376541e75759
SeanPaul <paul12999>
2021-03-15 16:22:15 +0800

    web ruby2.7 paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments, :dependent => :destroy
-  mount_uploader :image, ImageUploader
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x200#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_164745) do
+ActiveRecord::Schema.define(version: 2021_03_15_072936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,10 @@ ActiveRecord::Schema.define(version: 2021_03_04_164745) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.string "image"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
